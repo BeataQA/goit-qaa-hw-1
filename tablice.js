f191d20f7e6c40f4eecc7db840e3d465ef913f8a
BeataQA <162620630+BeataQA@users.noreply.github.com>
2024-06-23 20:00:50 +0200

    Add files via upload

---
+++ tablice.js
@@ -0,0 +1,10 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"]; 
+
+
+const numbers = [2, 6, 8, 3, 10, 4, 7]; 
+for (let i = 0; i < numbers.length; i++) { 
+    if (numbers[i] > 5) { 
+        console.log(numbers[i]); 
+
+    } 
+}
